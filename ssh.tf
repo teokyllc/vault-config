f019b3365e47e3c7eaad3535d8989cdc34fe4c82
Allan Taylor <allan@startmail.com>
2022-11-11 10:52:46 -0500

    Adding backend CA

--- ssh.tf
+++ ssh.tf
@@ -2,3 +2,7 @@ resource "vault_mount" "ssh" {
   path        = "ssh-client-signer"
   type        = "ssh"
 }
+
+resource "vault_ssh_secret_backend_ca" "backend_ca" {
+    backend = vault_mount.ssh.path
+}
